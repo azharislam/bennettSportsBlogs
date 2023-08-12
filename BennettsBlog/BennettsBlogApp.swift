fa6bb2b75bbebe31a50c12ffdb30c337c18e1e52
Azhar Islam <azhartislam@gmail.com>
2023-08-12 16:39:11 +0100

    Initial Commit

---
+++ BennettsBlog/BennettsBlogApp.swift
@@ -0,0 +1,17 @@
+//
+//  BennettsBlogApp.swift
+//  BennettsBlog
+//
+//  Created by Azhar Islam on 12/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct BennettsBlogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
